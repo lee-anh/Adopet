5bf54a31a87c011894120adad267e602a62e41be
shakh <saidovs@lafayette.edu>
2021-03-27 15:06:25 -0400

    DB schema created

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table pets (
+      name          CHAR NOT NULL,
+      species       CHAR NOT NULL,
+      breed         CHAR NOT NULL,
+      age           CHAR NOT NULL,
+      temperament   CHAR NOT NULL,
+      gender        CHAR NOT NULL);
