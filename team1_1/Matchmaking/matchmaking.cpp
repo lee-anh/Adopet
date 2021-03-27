ce99b2f76f89e83713436b3fbd2056cb0491b2a5
Claire Liu <claire.l.liu1@gmail.com>
2021-03-27 15:42:51 -0400

    open db

--- team1_1/Matchmaking/matchmaking.cpp
+++ team1_1/Matchmaking/matchmaking.cpp
@@ -4,3 +4,24 @@ Matchmaking::Matchmaking()
 {
 
 }
+
+
+void Matchmaking::findMatch(preferences p){
+
+}
+
+void Matchmaking::openDB(){
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    string fullName = "../../projectDB.sqlite";
+    db.setDatabaseName(QString::fromStdString(fullName));
+    if(!db.open()){
+        std::cerr << "Database does not open -- "
+                  << db.lastError().text().toStdString()
+                  << std::endl;
+
+        std::cerr << "  File -- " << fullName << std::endl;
+        exit(0);
+    } else {
+        std::cerr << "Opened database successfully\n";
+    }
+}
