fd0436eb70882b46ac39ecba398e92e6f0b2da79
Claire Liu <claire.l.liu1@gmail.com>
2021-03-27 14:57:07 -0400

    added sqlite file

---
+++ team1_1/Matchmaking/matchmaking.cpp
@@ -0,0 +1,6 @@
+#include "matchmaking.h"
+
+Matchmaking::Matchmaking()
+{
+
+}
---
+++ team1_1/Matchmaking/matchmaking.h
@@ -0,0 +1,28 @@
+#ifndef MATCHMAKING_H
+#define MATCHMAKING_H
+
+#include "../Adopter/adopter.h"
+//include animals
+#include <vector>
+
+using namespace std;
+
+class Matchmaking
+{
+public:
+    Matchmaking();
+
+    //search pet database once
+    //vector<pair<int, int>> database searching results.
+    //first int id , second int match score
+    //keep sorting in mind
+    //keep match score ties in mind (counter)
+
+
+    //search the vector of pairs and get 10 best animals
+
+    //search the database again for the 10 best animals
+    //vector<animals> of 10 best animals (highest scores)
+};
+
+#endif // MATCHMAKING_H
