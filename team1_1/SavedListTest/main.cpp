5988d0d104897ce5ba9c2192857f7f9f321f62e5
vuclaf <vuc@lafayette.edu>
2021-04-22 01:21:40 -0400

    added test for savedlisttest and gcov

--- team1_1/SavedListTest/main.cpp
+++ team1_1/SavedListTest/main.cpp
@@ -33,65 +33,110 @@ class SavedListTest : public ::testing::Test {
 
 
 
-     void SetUp() override {
-        SavedList* savedList = new SavedList("../../testDB.sqlite", "user1");
-        string age = "Young";
-        string name = "Alex";
-        string gender = "Male";
-        string species = "Cat";
-        string breed = "Siamese";
-        string temp = "Passive";
-        string gw = "Kids";
-        string sh = "Big";
-        string bio = "Lorem Ipsum";
-        string size = "Medium";
-
-        Pet* testPet1 = new Pet(name, species, breed, age, size, temp, gender, gw, sh, bio);
-        Pet* testPet2 = new Pet("John", species, breed, age, size, temp, gender, gw, sh, bio);
-        Pet* testPet3 = new Pet("Connor", species, breed, age, size, temp, gender, gw, sh, bio);
+     virtual void SetUp() override{
      }
 
-     void TearDown() override{
-
-     // Code here will be called immediately after each test (right
-
-     // before the destructor).
-
+     virtual void TearDown() override{
      }
 
      // Objects declared here can be used by all tests in the test case for Foo.
-     SavedList* savedList;
-     Pet* testPet1;
-     Pet* testPet2;
-     Pet* testPet3;
 };
 
 
 
 
-TEST_F(SavedListTest, SavingPets) {
+TEST(SavedListTest, SavingPets) {
+    SavedList* savedList = new SavedList("../../testDB.sqlite", "user1");
+    string age = "Young";
+    string name = "Alex";
+    string gender = "Male";
+    string species = "Cat";
+    string breed = "Siamese";
+    string temp = "Passive";
+    string gw = "Kids";
+    string sh = "Big";
+    string bio = "Lorem Ipsum";
+    string size = "Medium";
+    Pet* testPet1 = new Pet(name, species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet2 = new Pet("John", species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet3 = new Pet("Connor", species, breed, age, size, temp, gender, gw, sh, bio);
+
     savedList->savePet(*testPet1);
     savedList->savePet(*testPet2);
     savedList->savePet(*testPet3);
     vector<Pet> savedVec = savedList->getPetVec();
-    ASSERT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
-    ASSERT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
-    ASSERT_EQ(savedVec.at(2).getName(), "Connor") << savedVec.at(0).getName() << " should be Connor";
+    EXPECT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
+    EXPECT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
+    EXPECT_EQ(savedVec.at(2).getName(), "Connor") << savedVec.at(0).getName() << " should be Connor";
+
+    delete savedList;
+    delete testPet1;
+    delete testPet2;
+    delete testPet3;
 }
 
-TEST_F(SavedListTest, UnsavingPets){
+TEST(SavedListTest, UnsavingPets){
+    SavedList* savedList = new SavedList("../../testDB.sqlite", "user1");
+    string age = "Young";
+    string name = "Alex";
+    string gender = "Male";
+    string species = "Cat";
+    string breed = "Siamese";
+    string temp = "Passive";
+    string gw = "Kids";
+    string sh = "Big";
+    string bio = "Lorem Ipsum";
+    string size = "Medium";
+    Pet* testPet1 = new Pet(name, species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet2 = new Pet("John", species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet3 = new Pet("Connor", species, breed, age, size, temp, gender, gw, sh, bio);
+
     savedList->savePet(*testPet1);
     savedList->savePet(*testPet2);
     savedList->savePet(*testPet3);
     vector<Pet> savedVec = savedList->getPetVec();
-    ASSERT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
-    ASSERT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
-    ASSERT_EQ(savedVec.at(2).getName(), "Connor") << savedVec.at(0).getName() << " should be Connor";
+    EXPECT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
+    EXPECT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
+    EXPECT_EQ(savedVec.at(2).getName(), "Connor") << savedVec.at(0).getName() << " should be Connor";
     savedList->unsavePet(*testPet3);
     savedVec = savedList->getPetVec();
-    ASSERT_EQ(savedVec.size(), 2) << "Size of vector should be 2";
-    ASSERT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
-    ASSERT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
+    EXPECT_EQ(savedVec.size(), 2) << "Size of vector should be 2";
+    EXPECT_EQ(savedVec.at(0).getName(), "Alex") << savedVec.at(0).getName() << " should be Alex";
+    EXPECT_EQ(savedVec.at(1).getName(), "John") << savedVec.at(0).getName() << " should be John";
+
+    delete savedList;
+    delete testPet1;
+    delete testPet2;
+    delete testPet3;
+}
+
+TEST(SavedListTest, checkIsSaved){
+    SavedList* savedList = new SavedList("../../testDB.sqlite", "user1");
+    string age = "Young";
+    string name = "Alex";
+    string gender = "Male";
+    string species = "Cat";
+    string breed = "Siamese";
+    string temp = "Passive";
+    string gw = "Kids";
+    string sh = "Big";
+    string bio = "Lorem Ipsum";
+    string size = "Medium";
+    Pet* testPet1 = new Pet(name, species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet2 = new Pet("John", species, breed, age, size, temp, gender, gw, sh, bio);
+    Pet* testPet3 = new Pet("Connor", species, breed, age, size, temp, gender, gw, sh, bio);
+
+    savedList->savePet(*testPet1);
+    savedList->savePet(*testPet2);
+    savedList->savePet(*testPet3);
+    vector<Pet> savedVec = savedList->getPetVec();
+    bool result = savedList->isSavedPet(*testPet1);
+    EXPECT_EQ(result, true) << result << " should be 1";
+
+    delete savedList;
+    delete testPet1;
+    delete testPet2;
+    delete testPet3;
 }
 
 
